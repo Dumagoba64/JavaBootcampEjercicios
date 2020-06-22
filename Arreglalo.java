5869254775dd75d29bd673219330dd27358dd04e
Dulce María Gómez Barranco <dgomez@casystem.com.mx>
2020-06-22 18:44:17 -0500

    tarea de la clase del sabado 20 de junio 2020

--- Arreglalo.java
+++ Arreglalo.java
@@ -1,20 +1,32 @@
 public class Arreglalo {
-	public static void main() {
-		Cuadrado cuadrado = Cuadrado();
+	
+	public static void main(String[] args) {
+		
+		Cuadrado cuadrado = new Cuadrado();
+		System.out.println("Mi nombre es: "+cuadrado.nombre);
 
-		cuadrado.figura.rodar();
+		cuadrado.rodar();
 	}
 }
 
-class Cuadrado {
 
-	Figura figura;
+class Figura {
+	
+	String nombre;
+}
+
+
+class Cuadrado extends Figura {
 
 	public Cuadrado() {
 		iniciar();
 	}
 
-	private void iniciar() {
-		this.figura.nombre = cuadrado;
+	public void iniciar() {
+		this.nombre = "cuadrado";
+	}
+	
+	public void rodar() {
+		System.out.println("Un cuadrado rodando ...");
 	}
 }
